f6b0c6a5dacd09d4c1f802e53002eabfbbd1e73c
afa <afa@afalone.local>
2012-10-02 18:44:04 +0400

    module

--- lib/tmp_scan.rb
+++ lib/tmp_scan.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 require "ostruct"
+include NagiosLogger
 lines = []
 $stdin.each do |line|
  lines << parse_line(line)
@@ -9,19 +10,7 @@ lines.compact.each do |hsh|
  place_host(hsh, sites)
 end
 sort_sites(sites)
-events = []
-event = []
-sites.each do |k, v|
- v.each do |key, val|
-  val.each do | it |
-   event << it
-   if it[:level] == "OK"
-    events << event.dup
-    event.clear
-   end
-  end
- end
-end
+events = make_events(sites)
 levs = %w(OK WARNING CRITICAL UNKNOWN)
 events.each do |its|
  lvl = its.map{|it| levs.index(it[:level]) || 3 }.max
@@ -31,60 +20,79 @@ events.each do |its|
  puts "#{its.first[:host]}\t#{its.first[:service]}\t#{its.first[:stamp].to_i}\t#{its.first[:stamp].strftime("%Y-%m-%d %H:%M:%S")}\t#{levs[lvl]}\t#{len}\t#{its.first[:message]}"
 end
 
-def load_files(pref, mask)
- data = []
- Dir[File.join(pref, mask)].each do |fname|
-  data += load_file(fname, data)
- end
- data
-end
-
-def load_file(fname, data)
- File.open(fname, "r") do |f|
-  f.readlines.each do |line|
-   data << line
+module NagiosLogger
+ def load_files(pref, mask)
+  data = []
+  Dir[File.join(pref, mask)].each do |fname|
+   data += load_file(fname, data)
   end
+  data
  end
- data
-end
 
-def parse_line(line)
- data = {}
- line.force_encoding("KOI8-R").encode("UTF-8").scan(/^\[(\d+)\]\s+(.+)$/u) do |m|
-  unless m[0]
-   $stderr << m
-   return nil
+ def load_file(fname, data)
+  File.open(fname, "r") do |f|
+   f.readlines.each do |line|
+    data << line
+   end
   end
-  if m[1] !~ /^SERVICE ALERT/
-   return nil
-  end
-  data.merge(:stamp => Time.at(m[0].to_i))
-  m[1].scan(/SERVICE ALERT:\s+(.+?);(.+?);(.+?);(.+?);(.+?);(.+)$/) do |n|
-   data[:host] = n[0]
-   data[:service] = n[1]
-   data[:level] = n[2]
-   data[:type] = n[3]
-   data[:count] = n[4]
-   data[:message] = n[5]
-  end
+  data
  end
- data
-end
 
-def place_host(hsh, sites)
- unless sites.has_key?(hsh[:host])
-  sites[hsh[:host]] = {}
+ def make_events(sites)
+  events = []
+  event = []
+  sites.each do |k, v|
+   v.each do |key, val|
+    val.each do | it |
+     event << it
+     if it[:level] == "OK"
+      events << event.dup
+      event.clear
+     end
+    end
+   end
+  end
+  events
  end
- unless sites[hsh[:host]].has_key?(hsh[:service])
-  sites[hsh[:host]][hsh[:service]] = []
- end
- sites[hsh[:host]][hsh[:service]] << hsh
-end
 
-def sort_sites(sites)
- sites.each do |k, v|
-  v.each do |key, val|
-   val.sort!{|a, b| a[:stamp] <=> b[:stamp] }
+ def parse_line(line)
+  data = {}
+  line.force_encoding("KOI8-R").encode("UTF-8").scan(/^\[(\d+)\]\s+(.+)$/u) do |m|
+   unless m[0]
+    $stderr << m
+    return nil
+   end
+   if m[1] !~ /^SERVICE ALERT/
+    return nil
+   end
+   data.merge(:stamp => Time.at(m[0].to_i))
+   m[1].scan(/SERVICE ALERT:\s+(.+?);(.+?);(.+?);(.+?);(.+?);(.+)$/) do |n|
+    data[:host] = n[0]
+    data[:service] = n[1]
+    data[:level] = n[2]
+    data[:type] = n[3]
+    data[:count] = n[4]
+    data[:message] = n[5]
+   end
+  end
+  data
+ end
+
+ def place_host(hsh, sites)
+  unless sites.has_key?(hsh[:host])
+   sites[hsh[:host]] = {}
+  end
+  unless sites[hsh[:host]].has_key?(hsh[:service])
+   sites[hsh[:host]][hsh[:service]] = []
+  end
+  sites[hsh[:host]][hsh[:service]] << hsh
+ end
+
+ def sort_sites(sites)
+  sites.each do |k, v|
+   v.each do |key, val|
+    val.sort!{|a, b| a[:stamp] <=> b[:stamp] }
+   end
   end
  end
 end
--- spec/nagios_webbed_log_spec.rb
+++ spec/nagios_webbed_log_spec.rb
@@ -1,7 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "NagiosWebbedLog" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
+ it "should get /" do
+  get "/"
+  response.should be_success
+ end
 end
