ea74125d0931ad3c4e72dc2a0d61ab3adb612a58
afa <afa@afalone.local>
2012-10-04 19:07:22 +0400

    events

---
+++ lib/nagios_logger.rb
@@ -0,0 +1,81 @@
+module NagiosLogger
+ def load_file_list(pref)
+  Dir[File.join(pref, "*")].map{|n| File.basename(n) }
+ end
+
+ def load_files(pref, mask)
+  data = []
+  Dir[File.join(pref, mask)].each do |fname|
+   data += load_file(fname, data)
+  end
+  data
+ end
+
+ def load_file(fname, data)
+  File.open(fname, "r") do |f|
+   f.readlines.each do |line|
+    data << line
+   end
+  end
+  data
+ end
+
+ def make_events(sites)
+  events = []
+  event = []
+  sites.each do |k, v|
+   v.each do |key, val|
+    val.each do | it |
+     event << it
+     if it[:level] == "OK"
+      events << event.dup
+      event.clear
+     end
+    end
+   end
+  end
+  events
+ end
+
+ def parse_line(line)
+  data = {}
+  line.force_encoding("KOI8-R").encode("UTF-8").scan(/^\[(\d+)\]\s+(.+)$/u) do |m|
+   unless m[0]
+    $stderr << m
+    return nil
+   end
+   if m[1] !~ /^SERVICE ALERT/
+    return nil
+   end
+   data.merge(:stamp => Time.at(m[0].to_i))
+   m[1].scan(/SERVICE ALERT:\s+(.+?);(.+?);(.+?);(.+?);(.+?);(.+)$/) do |n|
+    data[:host] = n[0]
+    data[:service] = n[1]
+    data[:level] = n[2]
+    data[:type] = n[3]
+    data[:count] = n[4]
+    data[:message] = n[5]
+   end
+  end
+  data
+ end
+
+ def place_host(hsh, sites)
+  unless sites.has_key?(hsh[:host])
+   sites[hsh[:host]] = {}
+  end
+  unless sites[hsh[:host]].has_key?(hsh[:service])
+   sites[hsh[:host]][hsh[:service]] = []
+  end
+  sites[hsh[:host]][hsh[:service]] << hsh
+ end
+
+ def sort_sites(sites)
+  sites.each do |k, v|
+   v.each do |key, val|
+    val.sort!{|a, b| a[:stamp] <=> b[:stamp] }
+   end
+  end
+ end
+end
+
--- lib/nagios_webbed_log.rb
+++ lib/nagios_webbed_log.rb
@@ -1,19 +1,23 @@
 require 'rubygems'
 require 'sinatra/base'
-
+require File.expand_path(File.join(File.dirname(__FILE__), 'nagios_logger'))
+include NagiosLogger
 class NagiosWebbedLog < Sinatra::Base
 
   # This can display a nice status message.
   #
-  get "/:year/:monts/:day.csv" do
+  get "/:year/:month/:day.csv" do
+   get_params
+   load_dates
+   @events = load_events
+   haml :index
+  end
+
+  get "/:year/:month/:day" do
 
   end
 
-  get "/:year/:monts/:day" do
-
-  end
-
-  get "/:year/:monts" do
+  get "/:year/:month" do
   
   end
 
@@ -22,13 +26,50 @@ class NagiosWebbedLog < Sinatra::Base
   end
 
   get "/" do
-   load_arr
-   haml :index
+   if params[:day] && params[:day] != '0'
+    redirect "/#{params[:year]}/#{params[:month]}/#{params[:day]}"
+   elsif params[:month] && params[:month] != '0'
+    redirect "/#{params[:year]}/#{params[:month]}"
+   elsif params[:year] && params[:year] != '0'
+    redirect "/#{params[:year]}"
+   else
+    load_dates
+    @events = []
+    haml :index
+   end
   end
 
+ def load_events
+ end
+
+ def get_params
+  if params[:year] && (2000..2050).map(&:to_s).include?(params[:year])
+   @year = params[:year]
+  else
+   @year = "0"
+  end
+  if params[:month] && (1..12).map(&:to_s).include?(params[:month])
+   @month = params[:month]
+  else
+   @month = "0"
+  end
+  if params[:day] && (1..31).map(&:to_s).include?(params[:day])
+   @day = params[:day]
+  else
+   @day = "0"
+  end
+ end
+
+ def load_dates
+  p "---log location", ENV["NAGIOS_LOG"]
+  @names_list = load_file_list(ENV["NAGIOS_LOG"])
+  p "---names", @names_list
+  p "---split", @names_list.map{|n| n.split("-") }
+  @years = @names_list.map{|n| n.split("-") }.map{|n| n[3] }.uniq
+  p "---ye", @years
+  @days = @names_list.map{|n| n.split("-") }.map{|n| n[2] }.uniq
+  @months = @names_list.map{|n| n.split("-") }.map{|n| n[1] }.uniq
+ end
  def load_arr
-  @years = []
-  @months = []
-  @days = []
  end
 end
